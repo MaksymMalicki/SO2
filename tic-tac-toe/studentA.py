22674a092f44fbb405a37511abda2b00d6d78999
Jakub <jakub.fita3112@gmail.com>
2023-05-29 18:07:58 +0200

    ddd

--- tic-tac-toe/studentA.py
+++ tic-tac-toe/studentA.py
@@ -1,10 +1,26 @@
-array= [['_','_','_'],['_','_','_'],['_','_','_']]
-
 def print_board(array):
-    print(f'{array[0][1]}|{array[0][1]}|{array[0][2]}')
-    print('_____')
-    print(f'{array[1][1]}|{array[1][1]}|{array[1][2]}')
-    print('_____')
-    print(f'{array[2][1]}|{array[2][1]}|{array[2][2]}')
+    for row in array:
+        print(row)
 
+array = [['X','_','_'],['X','_','_'],['X','_','_'],['_','_','_']]
 print_board(array)
+
+def is_game_over(board):
+    for row in board:
+        if len(set(row)) == 1 and row[0] != '_':
+            return True
+
+    for col in range(3):
+        if len(set([board[row][col] for row in range(3)])) == 1 and board[0][col] != '_':
+            return True
+
+    if len(set([board[i][i] for i in range(3)])) == 1 and board[0][0] != '_':
+        return True
+    if len(set([board[i][2 - i] for i in range(3)])) == 1 and board[0][2] != '_':
+        return True
+
+    return False
+
+
+
+print(is_game_over(array))
